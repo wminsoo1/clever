f63e66535a45a2b4a0878117c1f13ff479be29bd
wminsoo1 <wminsoo1@naver.com>
2024-01-19 13:17:48 +0900

    Initial commit

---
+++ src/main/java/clever/cloning/CloningApplication.java
@@ -0,0 +1,13 @@
+package clever.cloning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clever/cloning/CloningApplicationTests.java
@@ -0,0 +1,13 @@
+package clever.cloning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
